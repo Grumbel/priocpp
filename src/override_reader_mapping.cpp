6a3fcd1ae6008698946b49d1f20b514d4f00e33d
Ingo Ruhnke <grumbel@gmail.com>
2020-10-11 14:54:55 +0200

    Fix typo in overrride_reader.hpp

--- src/override_reader_mapping.cpp
+++ src/override_reader_mapping.cpp
@@ -14,7 +14,7 @@
 // You should have received a copy of the GNU Lesser General Public License
 // along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-#include "overrride_reader.hpp"
+#include "override_reader_mapping.hpp"
 
 #include <set>
 
